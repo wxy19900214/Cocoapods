255422bd7bbc49c67b50d993a7fbd5323e3b3194
v_wxyawang <v_wxyawang@tencent.com>
2020-08-19 16:20:54 +0800

    第一次提交

---
+++ TestFramework/TestMethod.h
@@ -0,0 +1,19 @@
+//
+//  TestMethod.h
+//  TestFramework
+//
+//  Created by v_wxyawang on 2020/8/19.
+//  Copyright © 2020 v_wxyawang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestMethod : NSObject
+
+- (NSString *)TestObject;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestFramework/TestMethod.m
@@ -0,0 +1,20 @@
+//
+//  TestMethod.m
+//  TestFramework
+//
+//  Created by v_wxyawang on 2020/8/19.
+//  Copyright © 2020 v_wxyawang. All rights reserved.
+//
+
+#import "TestMethod.h"
+
+@implementation TestMethod
+
+- (NSString *)TestObject {
+    
+    NSString *str = @"调用了";
+    
+    return str;
+}
+
+@end
